54983fb0f018d80a62e967c09d4427bb24c4c785
eliobou <eliobou@gmail.com>
2019-02-25 17:06:56 +0100

    f

--- AndroidQuizz/app/src/main/java/javaClass/statistiques.java
+++ AndroidQuizz/app/src/main/java/javaClass/statistiques.java
@@ -1,4 +1,37 @@
 package javaClass;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.androidquizz.R;
+
 public class statistiques {
+    private int nbPartiesJouees;
+    private int nbReponsesCorrectes;
+
+    private final Context context;
+    private final SharedPreferences sharedPrefs;
+
+    public statistiques() {
+    }
+
+    public int getNbPartiesJouees() {
+        return nbPartiesJouees;
+    }
+
+    public void incrementerNbPartiesJouees() {
+        this.nbPartiesJouees++;
+    }
+
+    public int getNbReponsesCorrectes() {
+        SharedPreferences  localSettings = mContext.getSharedPreferences("FileName",
+                Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = localSettings.edit();
+
+        return nbReponsesCorrectes;
+    }
+
+    public void incrementerNbReponsesCorrectes() {
+        this.nbReponsesCorrectes++;
+    }
 }
