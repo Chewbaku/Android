10d4dd6980c9794b875096f64604c2cd5a2e651a
AMaugas <44227801+AMaugas@users.noreply.github.com>
2019-02-25 17:07:05 +0100

    implementation of GSON

--- AndroidQuizz/app/src/main/java/com/example/androidquizz/MainActivity.java
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.androidquizz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
