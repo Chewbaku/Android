8c3a318e54841a9764f9c177578035aa687f7895
Hectorght <47627684+Hectorght@users.noreply.github.com>
2019-02-26 09:21:31 +0100

    layout_refait
    j'ai changé le layout du menu

---
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/activity_menu.java
@@ -0,0 +1,15 @@
+package com.example.androidquizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.androidquizz.R;
+
+public class activity_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
