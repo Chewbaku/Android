a20c05d2f8687afb080912070628d1b52a00adda
eliobou <eliobou@gmail.com>
2019-02-26 10:19:36 +0100

    clean project

---
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/lobby.java
@@ -0,0 +1,13 @@
+package com.example.androidquizz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class lobby extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lobby);
+    }
+}
