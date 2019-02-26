12ea756eafaf851f944bfc0347c7c8dddf87a758
eliobou <eliobou@gmail.com>
2019-02-26 11:36:02 +0100

    bouton pour changer de vue

--- AndroidQuizz/app/src/main/java/com/example/androidquizz/lobby.java
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/lobby.java
@@ -1,7 +1,10 @@
 package com.example.androidquizz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class lobby extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class lobby extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+
+        Button buttonStart;
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), question.class);  //Lancer l'activité DisplayVue
+                startActivity(intent);    //Afficher la vue
+
+            }
+        });
     }
 }
