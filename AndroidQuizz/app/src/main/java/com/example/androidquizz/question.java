5c606767dc8c9bdb3fcded2aa9b1175800e187a6
Anthony Carlier <anthony.carlier@isen-lille.fr>
2019-03-01 00:17:12 +0100

    ajout du timer qui kill la question

--- AndroidQuizz/app/src/main/java/com/example/androidquizz/question.java
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/question.java
@@ -1,13 +1,43 @@
 package com.example.androidquizz;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class question extends AppCompatActivity {
+
+    MyTimer myCountDownTimer;
+    TextView mCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        mCount = (TextView)findViewById(R.id.count);
+        myCountDownTimer = new MyTimer(10000, 1000);
+        myCountDownTimer.start();
+    }
+
+    public class MyTimer extends CountDownTimer {
+
+        public MyTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+            int progress = (int) (millisUntilFinished/1000);
+
+            mCount.setText(String.valueOf(progress+1));
+        }
+
+        @Override
+        public void onFinish() {
+            mCount.setText("FIN");
+            finish();
+        }
     }
 }
