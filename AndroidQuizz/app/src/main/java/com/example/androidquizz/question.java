80b66325dd25564f5375e0dc2a5620a88e46ad73
Unknown <benjamin.legrand@isen.yncrea.fr>
2019-02-28 16:15:53 +0100

    question
    j'avais oublié

--- AndroidQuizz/app/src/main/java/com/example/androidquizz/question.java
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/question.java
@@ -1,20 +1,79 @@
 package com.example.androidquizz;
 
-import android.os.Bundle;
+import com.google.gson.annotations.SerializedName;
 
-import androidx.appcompat.app.AppCompatActivity;
+import java.util.Arrays;
 
-public class question extends AppCompatActivity {
+public class Question {
+    @SerializedName("question")
+    private final String mQuestion;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question);
+    @SerializedName("reponses")
+    private final String[] mResponses;
+
+    @SerializedName("reponse_correcte")
+    private final String mAnswer;
+
+    @SerializedName("theme")
+    private final String mTheme;
+
+    @SerializedName("difficulte")
+    private final String mDifficulty;
+
+    private int correctAnswer;
+    private int difficulty;
+
+    public Question(String question, String[] responses, String answer, String theme, String difficulty) {
+        mQuestion = question;
+        mResponses = responses;
+        mAnswer = answer;
+        mTheme = theme;
+        mDifficulty = difficulty;
     }
 
-    //empêche de quitter la vue via le bouton retour
+    public void update() {
+        this.correctAnswer = Integer.parseInt(this.getAnswer());
+        this.difficulty = Integer.parseInt(this.getStringDifficulty());
+    }
+
+    public String getQuestion() {
+        return this.mQuestion;
+    }
+
+    public int getCorrectAnswer() {
+        return this.correctAnswer;
+    }
+
+    public int getIntDiffuculty() {
+        return this.difficulty;
+    }
+
+    public String getAnswer() {
+        return this.mAnswer;
+    }
+
+    public String getTheme() {
+        return this.mTheme;
+    }
+
+    public String getStringDifficulty() {
+        return this.mDifficulty;
+    }
+
+    public String[] getResponses() {
+        return this.mResponses;
+    }
+
     @Override
-    public void onBackPressed() {
-        return;
+    public String toString() {
+        return "\nQuestion{\n" +
+                "mQuestion=" + mQuestion + '\n' +
+                "mResponses=" + Arrays.toString(mResponses) + '\n' +
+                "mAnswer=" + mAnswer + '\n' +
+                "mTheme=" + mTheme + '\n' +
+                "mDifficulty=" + mDifficulty + '\n' +
+                "correctAnswer=" + correctAnswer + '\n' +
+                "difficulty=" + difficulty + '\n' +
+                "}\n";
     }
 }
