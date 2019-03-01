dacf056f3489d74599d3dcf76b9441b5b754e661
Anthony Carlier <anthony.carlier@isen-lille.fr>
2019-03-01 01:30:49 +0100

    Version plus compacte du timer

--- AndroidQuizz/app/src/main/java/com/example/androidquizz/question.java
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/question.java
@@ -7,36 +7,34 @@ import android.widget.TextView;
 
 public class question extends AppCompatActivity {
 
-    MyTimer myCountDownTimer;
-    TextView mCount;
+    Timer mTimer;
+    TextView mTimerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
 
-        mCount = (TextView)findViewById(R.id.count);
-        myCountDownTimer = new MyTimer(10000, 1000);
-        myCountDownTimer.start();
+        mTimerText = (TextView)findViewById(R.id.timer);
+        mTimer = new Timer(10000, 1000);
+        mTimer.start();
     }
 
-    public class MyTimer extends CountDownTimer {
+    public class Timer extends CountDownTimer {
 
-        public MyTimer(long millisInFuture, long countDownInterval) {
+        public Timer(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
         }
 
         @Override
         public void onTick(long millisUntilFinished) {
-
             int progress = (int) (millisUntilFinished/1000);
-
-            mCount.setText(String.valueOf(progress+1));
+            mTimerText.setText(String.valueOf(progress+1));
         }
 
         @Override
         public void onFinish() {
-            mCount.setText("FIN");
+            mTimerText.setText("FIN");
             finish();
         }
     }
