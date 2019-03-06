bd80a659c7dd18742ec362ff0e411fe7b2862ada
AMaugas <44227801+AMaugas@users.noreply.github.com>
2019-03-06 15:20:13 +0100

    Suppression Quizz.java (inutile)

--- AndroidQuizz/app/src/main/java/com/example/androidquizz/QuizzActivity.java
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/QuizzActivity.java
@@ -4,16 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.List;
@@ -36,15 +39,15 @@ public class QuizzActivity extends AppCompatActivity {
         this.mGson = gsonBuilder.create();
 
         this.mRequestQueue = Volley.newRequestQueue(getApplicationContext());
+
         this.fetchPost();
     }
 
     private void fetchPost() {
-        StringRequest request = new StringRequest(Request.Method.GET, ENDPOINT, onQuestionsLoaded, onQuestionsError);
-
+        StringRequest request = new StringRequest(Request.Method.GET,ENDPOINT, onQuestionsLoaded, onQuestionsError);
         this.mRequestQueue.add(request);
     }
-
+    
     private final Response.Listener<String> onQuestionsLoaded = new Response.Listener<String>() {
         @Override
         public void onResponse(String response) {
@@ -65,6 +68,8 @@ public class QuizzActivity extends AppCompatActivity {
             launchQuizz(questions1);
 
         }
+
+
     };
 
     private final Response.ErrorListener onQuestionsError = new Response.ErrorListener() {
