0580af906b8f9565f8b9a4d7b381062e43cee3dd
eliobou <eliobou@gmail.com>
2019-03-06 15:55:08 +0100

    add share button

--- AndroidQuizz/app/src/main/java/com/example/androidquizz/ScoreActivity.java
+++ AndroidQuizz/app/src/main/java/com/example/androidquizz/ScoreActivity.java
@@ -1,6 +1,10 @@
 package com.example.androidquizz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScoreActivity extends AppCompatActivity {
@@ -9,5 +13,25 @@ public class ScoreActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        Button btnPartager = findViewById(R.id.partager);
+
+        btnPartager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareIt();
+            }
+        });
+    }
+
+    //partager
+    private void shareIt() {
+        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+        sharingIntent.setType("text/plain");
+        String shareBody = "Here is the share content body";
+
+        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Score Android Quizz");
+        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+        startActivity(Intent.createChooser(sharingIntent, "Partager via"));
     }
 }
